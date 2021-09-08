a7874169d033efa9d4394992efe70b9799facff3
shc0801 <shc123987@naver.com>
2021-09-08 16:39:35 +0900

    feat: add schedule calender

---
+++ src/components/Schedule/Calender/CalenderDate.tsx
@@ -0,0 +1,127 @@
+import React, {
+    ReactElement,
+    useMemo,
+    useCallback,
+    memo,
+    useEffect
+  } from "react";
+  import { useDispatch } from "react-redux";
+  
+  import * as S from "./style";
+  import Scheduler from "./Scheduler";
+  
+  import { padNum } from "../../../../lib/utils";
+  import { setSelectedDate } from "../../../../modules/action/main";
+  import useCustomSelector from "../../../../lib/hooks/useCustomSelector";
+  
+  interface Props {}
+  
+  const CalendarDate: React.FC<Props> = () => {
+    const dispatch = useDispatch();
+    const { schedulerDate } = useCustomSelector().main;
+  
+    const handleClickDate = (target: HTMLDivElement) => {
+      const id = target.dataset.id;
+      const classList = target.classList;
+      const children = target.parentNode.children;
+  
+      if (classList.contains("selected")) {
+        dispatch(setSelectedDate(""));
+        classList.remove("selected");
+        return;
+      }
+  
+      Array.from(children).map(el => {
+        el.classList.remove("selected");
+      });
+  
+      dispatch(setSelectedDate(id));
+      classList.add("selected");
+    };
+  
+    const getDateJSX = (
+      children: number,
+      id: string = "",
+      styling: string = ""
+    ): ReactElement => {
+      if (!styling) {
+        return (
+          <S.CalendarDate key={id} className={styling}>
+            <S.CalendarDaySpan>{padNum(children)}</S.CalendarDaySpan>
+          </S.CalendarDate>
+        );
+      }
+  
+      return (
+        <S.CalendarDate
+          key={id}
+          data-id={id}
+          className={styling}
+          onClick={e => handleClickDate(e.currentTarget)}
+        >
+          <S.CalendarDaySpan>{padNum(children)}</S.CalendarDaySpan>
+        </S.CalendarDate>
+      );
+    };
+  
+    const printCalendar = useCallback((): ReactElement[] => {
+      const yearCopy = schedulerDate.getFullYear();
+      const monthCopy = schedulerDate.getMonth() + 1;
+      const fixedMonth = padNum(monthCopy);
+      const lastDay = new Date(yearCopy, monthCopy, 0).getDate();
+      const firstDay = new Date(yearCopy, monthCopy - 1, 1).getDay();
+      const jsx: ReactElement[] = [];
+      let startDayCount = 1;
+      let nextDayCount = 1;
+  
+      for (let i = 0; i < 6; i += 1) {
+        for (let j = 0; j < 7; j += 1) {
+          if (i === 0 && j < firstDay) {
+            const prevLastDate = new Date(yearCopy, monthCopy - 1, 0).getDate();
+  
+            jsx.push(getDateJSX(prevLastDate - firstDay + j + 1, `${j}`));
+          } else if (i >= 0 && startDayCount <= lastDay) {
+            const date = `${yearCopy}-${fixedMonth}-${padNum(startDayCount)}`;
+            const t = new Date();
+            const y = t.getFullYear();
+            const m = t.getMonth() + 1;
+            const d = t.getDate();
+  
+            date === `${y}-${padNum(m)}-${padNum(d)}`
+              ? jsx.push(getDateJSX(startDayCount, date, "curr today"))
+              : jsx.push(getDateJSX(startDayCount, date, "curr"));
+            startDayCount += 1;
+          } else {
+            const date: string =
+              +fixedMonth + 1 > 12
+                ? `${+yearCopy + 1}-${1}-${padNum(nextDayCount)}`
+                : `${yearCopy}-${+fixedMonth + 1}-${padNum(nextDayCount)}`;
+  
+            jsx.push(getDateJSX(nextDayCount, date));
+            nextDayCount += 1;
+          }
+        }
+      }
+      return jsx;
+    }, [schedulerDate]);
+  
+    const memoizedCalendar = useMemo<ReactElement[]>(() => {
+      return printCalendar();
+    }, [schedulerDate]);
+  
+    useEffect(() => {
+      return () => {
+        dispatch(setSelectedDate(""));
+      };
+    }, []);
+  
+    return (
+      <>
+        {memoizedCalendar}
+        <Scheduler />
+      </>
+    );
+  };
+  
+  export default memo(CalendarDate);
+  
---
+++ src/components/Schedule/style.ts
@@ -0,0 +1,108 @@
+import styled from "styled-components";
+
+export const Calendar = styled.div`
+  position: relative;
+  display: flex;
+  flex-wrap: wrap;
+  margin: auto;
+`;
+
+export const CalendarLoadingWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 480px;
+`;
+
+export const CalendarDTemp = styled.div`
+  box-sizing: border-box;
+  display: flex;
+  align-items: flex-start;
+  justify-content: flex-end;
+  width: calc(100% / 7);
+  padding: 4px 8px;
+  font-size: 12px;
+`;
+
+export const CalendarDate = styled(CalendarDTemp)`
+  position: relative;
+  height: 80px;
+  border: 1.5px solid transparent;
+  box-sizing: border-box;
+  color: rgba(0, 0, 0, 20%);
+  cursor: default;
+  z-index: 10;
+  &.curr {
+    color: black;
+    cursor: pointer;
+  }
+  &.selected {
+    border: 1.5px solid #5323b2;
+  }
+  &.today > span {
+    color: white;
+    background-color: #ff5555;
+  }
+`;
+
+export const CalendarDay = styled(CalendarDTemp)`
+  align-items: flex-end;
+  height: 30px;
+  border-bottom: 2px solid #dddddd;
+  color: rgb(75, 75, 75);
+  font-weight: bold;
+  &.dayOfWeek {
+    border-color: #ff5555;
+  }
+`;
+
+export const CalendarDaySpan = styled.span`
+  width: 18px;
+  height: 18px;
+  line-height: 18px;
+  padding: 2px;
+  border-radius: 50%;
+  text-align: center;
+`;
+
+interface ICalendarBar {
+  overlap: number;
+  weekOfMonth: number;
+  day: number;
+  backgroundColor: string;
+  isStart: boolean;
+  isEnd: boolean;
+}
+
+export const CalendarBar = styled.div<ICalendarBar>`
+  position: absolute;
+  top: ${({ weekOfMonth, overlap }) => {
+    const lineHeight = 17;
+    const weekHeight = 80;
+    const defaultTop = 60;
+    return lineHeight * overlap + weekHeight * (weekOfMonth - 1) + defaultTop;
+  }}px;
+  left: ${({ day }) => `calc(100% / 7 * ${day});`};
+  width: calc(100% / 7);
+  height: 15px;
+  background-color: ${({ backgroundColor }) => backgroundColor};
+  padding-left: 2px;
+  border-left: ${({ isStart }) => isStart && "1px solid white"};
+  border-right: ${({ isEnd }) => isEnd && "1px solid white"};
+  color: white;
+  font-size: 8px;
+  box-sizing: border-box;
+  cursor: pointer;
+  &.prev {
+    background-color: gray;
+  }
+`;
+
+export const CalendarBarDetail = styled.p`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`;
