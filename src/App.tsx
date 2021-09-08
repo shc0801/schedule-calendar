1fb3552aba7e760f6b6b4a3a4d0e0cca8ba802f0
shc0801 <shc123987@naver.com>
2021-09-08 16:40:40 +0900

    feat: nomal typescript setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import Router from "./Router";
+import { GlobalStyle } from './GlobalStyle';
+import { ToastContainer } from 'react-toastify';
+// import { useDispatch } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App:React.FC = () => {
+
+  return(
+    <BrowserRouter>
+      <GlobalStyle />
+      <ToastContainer />
+      <Router />
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/GlobalStyle.ts
@@ -0,0 +1,79 @@
+import {
+    createGlobalStyle,
+    GlobalStyleComponent,
+    DefaultTheme,
+  } from 'styled-components';
+  
+  export const GlobalStyle: GlobalStyleComponent<
+    {},
+    DefaultTheme
+  > = createGlobalStyle`
+  * {
+    font-family: 'Noto Sans', 'Noto Sans KR', sans-serif;
+    box-sizing: border-box;
+  }
+  
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  b, u, i, center,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td,
+  article, aside, canvas, details, embed,
+  figure, figcaption, footer, header, hgroup,
+  menu, nav, output, ruby, section, summary,
+  time, mark, audio, video, button {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+  
+  article, aside, details, figcaption, figure,
+    footer, header, hgroup, menu, nav, section {
+    display: block;
+  }
+  
+  ol, ul {
+    list-style: none;
+  }
+  
+  blockquote, q {
+    quotes: none;
+  }
+  
+  a {
+    text-decoration: none;
+  }
+  
+  img {
+    cursor: pointer;
+  }
+  
+  input:focus,
+  select:focus,
+  textarea:focus,
+  button:focus {
+    outline: none;
+  }
+  
+  button {
+    cursor: pointer;
+    font-family: 'Noto Sans', sans-serif;
+  }
+  
+  /* #root {
+    width: 678px;
+    margin: 50px auto;
+    box-shadow: 0 3px 4px #121212;
+  } */
+  `;
+  
+  export const globalColor = '#E669FF';
+  export const globalBackColor = '#923EA3';
+  export const globalButtonColor = '#D96CF0';
+  export const globalComplementaryColor = '#6EA329';
+  export const globalComplementaryBackColor = '#B6F06C';
+  
