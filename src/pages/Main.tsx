268912ff4b77bee668687f0548bc7a79496cc14b
shc0801 <shc123987@naver.com>
2021-09-08 16:40:02 +0900

    feat: add MainPage

---
+++ src/pages/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from "../components/Main/Header";
+
+// import { Header, Sidebar } from "../../components";
+
+const MainPage: React.FC = () => {
+
+  return (
+    <>
+        <Header />
+    </>
+  );
+};
+
+export default MainPage;
