ffa6d6ac650858ad8a359c0407c2dace70cf4f93
shc0801 <shc123987@naver.com>
2021-09-24 13:43:14 +0900

    fix: delete router and add redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, { FC } from 'react';
 import { Switch, Router, Route, Redirect } from "react-router-dom";
 import { history } from "./modules/store";
 import { GlobalStyle, GlobalContainer } from "./GlobalStyle";
 import {
   Navigation
 } from "./components";
+import {
+  MainPage,
+} from './pages';
+import {
+  userRoute
+} from "./lib/static";
 import { ToastContainer } from 'react-toastify';
-// import { useDispatch } from 'react-redux';
 
-const App:React.FC = () => {
 
-  return(
+const App: FC<{}> = () => {
+
+  return (
     <GlobalContainer>
       <GlobalStyle />
       <ToastContainer />
       <Router history={history}>
-        <Navigation />
+        <Navigation routeData={userRoute} />
+        <Switch>
+          <Route path="/home" component={MainPage} />
+          <Redirect path="/" to="/home" />
+        </Switch>
       </Router>
     </GlobalContainer>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Helmet, HelmetProvider } from 'react-helmet-async';
+import { HelmetProvider } from 'react-helmet-async';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './App';
+import rootReducer from './modules/reducer';
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
