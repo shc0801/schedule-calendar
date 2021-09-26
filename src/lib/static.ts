f45b34d4c9c58d8e49d23e3d8f5e6a646aea8afa
shc0801 <shc123987@naver.com>
2021-09-27 03:52:21 +0900

    fix: change routeData -> routeDataType

--- src/lib/static.ts
+++ src/lib/static.ts
@@ -1,17 +1,19 @@
 import { NavItem } from "../components/Navigation/Navigation"
 
-export interface RouteData {
+export interface routeDataType {
   data: NavItem[];
 }
 
-export const userRoute: RouteData = {
+export const routeData: routeDataType = {
   data: [
     {
       name: "홈",
+      enName: "home",
       route: "/home"
     },
     {
       name: "공지",
+      enName: "notice",
       route: "/notice"
     }
   ]
