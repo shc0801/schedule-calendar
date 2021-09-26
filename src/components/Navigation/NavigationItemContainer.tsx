42bebaab43ced4882b77fc66c9b5746c3e189db0
shc0801 <shc123987@naver.com>
2021-09-27 03:51:21 +0900

    feat: grap in routeData

--- src/components/Navigation/NavigationItemContainer.tsx
+++ src/components/Navigation/NavigationItemContainer.tsx
@@ -1,21 +1,25 @@
-import React, { FC } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import RootState from "../../modules/reducer/index"
-import styled from 'styled-components';
+import React, { FC } from "react";
+import { useSelector } from "react-redux";
+import RootState from "../../modules/reducer/index";
+import styled from "styled-components";
+import { routeDataType } from "../../lib/static";
+import NavigationItem from "./NavigationItem";
 // import NavigationItem from './NavigationItem';
 
-const Nav = styled.nav`
+const Container = styled.nav`
   display: flex;
   flex-direction: column;
 
-  padding-top: 50px; 
+  padding-top: 50px;
 
-  & > a { 
-    font-size: 1.2em; 
+  & > a {
+    font-size: 1.2em;
     padding: 15px 0;
   }
-  & > a:nth-child(2) { color: #000 } 
-`
+  & > a:nth-child(2) {
+    color: #000;
+  }
+`;
 
 export interface NavItem {
   name: string;
@@ -23,17 +27,25 @@ export interface NavItem {
 }
 
 interface Props {
-  navItem: NavItem[];
+  routeData: routeDataType;
 }
 
-const NavigationItemContainer: FC<Props> = ({ navItem }) => {
-  const url = useSelector((state: typeof RootState) => state);
-  console.log(url);
-  const dispatch = useDispatch();
+const NavigationItemContainer: FC<Props> = ({ routeData }) => {
+  const url = useSelector(
+    (state: ReturnType<typeof RootState>) => state.page.page
+  );
+
   return (
-    <Nav>
-      
-    </Nav>
+    <Container>
+      {routeData.data.map((item, idx) => (
+        <NavigationItem
+          key={idx}
+          name={item.name}
+          route={item.route}
+          isActive={url === item.enName}
+        />
+      ))}
+    </Container>
   );
 };
 
