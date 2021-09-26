8af4e7e91dda5dad26a2ea635e1c27e143a2b931
shc0801 <shc123987@naver.com>
2021-09-27 03:50:38 +0900

    feat: click pageMove

--- src/components/Navigation/NavigationItem.tsx
+++ src/components/Navigation/NavigationItem.tsx
@@ -1,13 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { FC } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
 import styled from 'styled-components';
+import { pageMove } from '../../modules/actions';
 
+const Container = styled.div`
+  width: 350px;
+  background-color: #687af8;
 
-const NavigationItemContainer: React.FC = () => {
+  color: #fff;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  padding: 80px 50px;
+`
+
+interface Props {
+  name: string;
+  route: string;
+  isActive: boolean;
+}
+
+const NavigationItemContainer: FC<Props> = ({
+  name,
+  route,
+  isActive               
+}) => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const onClick = () => {
+    if(history.location.pathname === route) return;
+    dispatch(pageMove({page: route}));
+    console.log(route);
+    history.push(route);
+  };
+
   return (
-      <>
-        <Link to="/">Schedule Page</Link>
-      </>
+    <Container onClick={onClick}>
+      asdasdasd
+    </Container>
   );
 };
 
