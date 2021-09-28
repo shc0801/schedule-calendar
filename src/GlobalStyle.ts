9c1b84f90be5d89c6ed533a15d46543c4cb09171
shc0801 <shc123987@naver.com>
2021-09-29 02:38:30 +0900

    feat: add html, body overflow: hidden

--- src/GlobalStyle.ts
+++ src/GlobalStyle.ts
@@ -13,7 +13,7 @@ export const GlobalStyle: GlobalStyleComponent<
     box-sizing: border-box;
     color: inherit;
   }
-  
+  html, body { overflow: hidden; }
   html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
