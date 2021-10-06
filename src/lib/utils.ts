ff55d757da1116fd30c70719def1fb3a8f350e5b
shc0801 <shc123987@naver.com>
2021-10-06 15:46:00 +0900

    fix: func

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 import { PageState } from "../modules/reducer/page";
 
 export const getNavUrl = (url: string): PageState => {
-  const page = url.split("/")[1] === undefined ? url : url.split("/")[1];
+  const page = url.split("/")[1] === undefined ? "home" : url.split("/")[1];
   return { page: page }
 };
