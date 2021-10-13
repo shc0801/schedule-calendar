59af24a7964d0309670f24f5ec3dc9a7920c63d4
shc0801 <shc123987@naver.com>
2021-10-14 02:24:53 +0900

    feat: add EditForm import

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import Schedule from './Main/Schedule/Schedule';
 import Navigation from './Navigation/Navigation';
+import Calendar from './Main/Calendar/Calendar';
+import EditForm from './Edit/EditForm';
 
 export {
     Schedule,
-    Navigation
+    Navigation,
+    Calendar,
+    EditForm
 };
