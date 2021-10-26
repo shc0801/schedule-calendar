0811e5d7a31bc156b6a631e0e6520affabfc02f4
shc0801 <shc123987@naver.com>
2021-10-27 05:25:27 +0900

    feat: add loginPage, registerPage staticData

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,20 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { HelmetProvider } from 'react-helmet-async';
-import { createStore } from 'redux';
-import { Provider } from 'react-redux';
-import App from './App';
-import rootReducer from './modules/reducer';
+import React from "react";
+import ReactDOM from "react-dom";
+import { HelmetProvider } from "react-helmet-async";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
+import rootReducer from "./modules/reducer";
 
 const store = createStore(rootReducer);
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <HelmetProvider>
-        <App />
-      </HelmetProvider>
-    </React.StrictMode>
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
